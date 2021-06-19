2fc843b3c2ed1ec049974802e70b4a6c00ce85bb
adnan <adnanquisar0@gmail.com>
2021-06-19 15:17:11 +0100

    final challenge 2 - fib

--- finals_challenge_2.py
+++ finals_challenge_2.py
@@ -1,8 +1,8 @@
 import math
+
 def is_fibonacci(input):
   phi = 0.5 + 0.5 * math.sqrt(5.0)
   a = phi * input
-
 
   if(input >= 0 and input <= 2147483647 and (input % 1) == 0):  
     return input == 0 or abs(round(a) - a) < 1.0 / input   
@@ -10,5 +10,5 @@ def is_fibonacci(input):
     return False
 
 if __name__=='__main__':
-    input = 0
-    print("Fib: ", is_fibonacci(input))
+  input = 0
+  print("Fib: ", is_fibonacci(input))
